b8d8da8b0289990bb2e6684a8ff9a1a54482ac91
Jazenn <jason.griffioen@student.hu.nl>
2025-03-20 12:23:07 +0100

    Implemented system where environ_inf is affected by nr_solar panels in neighbourhood.

--- environment.py
+++ environment.py
@@ -6,10 +6,15 @@ class Environment:
         self.behavioral_inf = behavioral_inf
 
     
-    def change_influence(self,):
-        self.environmental_inf += random.uniform(-0.15, 0.15)
+    def change_influence(self, households: list):
+        nr_solarpanels = 0
+        for household in households:
+            if household.solar_panels:
+                nr_solarpanels += 1
+                
+        self.environmental_inf = nr_solarpanels / (len(households) * 2)
         self.behavioral_inf += random.uniform(-0.15, 0.15)
         return self.environmental_inf, self.behavioral_inf
     
     def print_values(self,):
-        print(f"Environmental influence: {self.environmental_inf:.2f}, Behavioral influence: {self.behavioral_inf:.2f}\n")
+        print(f"Environmental influence: {self.environmental_inf:.2f}, Behavioral influence: {self.behavioral_inf:.2f}\n\n")
