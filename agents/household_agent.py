2ec45ef969796d93be9a0a725ecb81795bda071e
Jazenn <jason.griffioen@student.hu.nl>
2025-03-20 12:23:41 +0100

    Added print func and household id.

--- agents/household_agent.py
+++ agents/household_agent.py
@@ -1,8 +1,12 @@
 from agents.agent import Agent
 
 class Household(Agent):
-    def __init__(self, solar_panels: bool):
+    def __init__(self, solar_panels: bool, id: int):
         super().__init__()
+        self.id = id
         self.solar_panels = solar_panels
+
+    def print_values(self,):
+        print(f"Household {self.id}, Solar panels installed: {self.solar_panels}")
 
     
