a20295440687c98e47974a5460f36da431090421
Jazenn <jason.griffioen@student.hu.nl>
2025-03-20 12:12:48 +0100

    Added better console log readability, added custom household creation.

--- main.py
+++ main.py
@@ -8,26 +8,42 @@ import random
 def gen_random_value(range_min: float, range_max: float):
     return random.uniform(range_min, range_max) 
 
-def main(iter: int, nr_residents: int=1, info_dump=False):
+def main(iter: int, nr_residents: int=1, nr_households: int=1, info_dump=False):
+    residents = []
+    households = []
     environment = Environment(gen_random_value(-0.1, 0.2), gen_random_value(-0.1, 0.2))
+
     if info_dump:
         print("Environment created with starting values:")
         environment.print_values()
 
-    residents = []
+    for i in range(nr_households):
+        households.append(Household(False, i))
+        if info_dump:
+            households[i].print_values()
+
+    if info_dump:
+        print("\n\n")
+            
     for i in range(nr_residents):
-        residents.append(Resident(i + 1, gen_random_value(-0.1, 0.2), gen_random_value(0.9, 1.2), gen_random_value(0.9, 1.2), gen_random_value(0.9, 1.2)))
+        residents.append(Resident(i, gen_random_value(-0.1, 0.2), gen_random_value(0.9, 1.2), gen_random_value(0.9, 1.2), gen_random_value(0.9, 1.2)))
         if info_dump:
             print(f"Resident created with starting values:")
             residents[i].print_values()
+
+    if info_dump:
+        print("\n\n")
+
     for i in range(iter):
-        print(f"Year: {i}")
-        influence = environment.change_influence()
+        print(f"Year: {i + 1}")
+        influence = environment.change_influence(households)
         for resident in residents:
             if resident.solar_panels == False:
                 resident.calc_decision(0.5, influence, info_dump)
         for resident in residents:
             if resident.solar_panels == True:
+                households[resident.id].solar_panels = True
                 print(f"Resident {resident.id} got solar panels!")
+        print("\n")
 
-main(10, 5, info_dump=True)
+main(20, 5, 5, info_dump=True)
