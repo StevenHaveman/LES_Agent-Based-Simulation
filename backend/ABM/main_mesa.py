9f35e190a67d301d477fc2110bb2a51a8b95a68e
Jazenn <jason.griffioen@student.hu.nl>
2025-04-22 12:41:42 +0200

    Changed logic so print statements are more inline with simulation flow.

--- backend/ABM/main_mesa.py
+++ backend/ABM/main_mesa.py
@@ -11,15 +11,13 @@ def run_simulation(nr_households=10, nr_residents=10, simulation_years=30):
         print(f"Environmental Influence: {model.environmental_inf}")
         print(f"Solar Panel Price: {model.solarpanel_price:.2f}")
 
+        model.step()
+
         print(f"\nEnd of Year {year + 1}:")
         print(f"  Decisions this year: {model.decided_residents}")
         print(f"  Current Environment State:") # Show state after year using __str__
         print(model)
         print("-" * 40)
-
-        model.step()
-
-
 
     return simulation_output
 
