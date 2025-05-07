90197c48d8baeec3cc5c4b8717e3fb58a24b50ca
Jazenn <jason.griffioen@student.hu.nl>
2025-05-07 14:35:09 +0200

    Started working on config for easily adjustabel params.

---
+++ backend/ABM/config.py
@@ -0,0 +1,12 @@
+config_default = {
+    # Main simulation startup parameters
+    "nr_households": 10,
+    "nr_residents": 10,
+    "simulation_years": 30,
+
+    # Environment parameters
+    "environmental_influence": 0.0,
+    "solar_panel_price": 410,
+    "energy_price": 0.32,
+    
+}
