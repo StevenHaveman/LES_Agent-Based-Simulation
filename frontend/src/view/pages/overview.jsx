c58960cab45e6556272ea9310d6a8d44aa544ce5
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2025-06-02 14:17:35 +0200

    #25,#26,#28

--- frontend/src/view/pages/overview.jsx
+++ frontend/src/view/pages/overview.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {createRoute} from "@tanstack/react-router";
 import {rootRoute} from "./root";
 import Navbar from "../components/Navbar.jsx";
@@ -6,18 +6,36 @@ import "../styles/overviewpage.css"
 import "../styles/globalPageStyles.css";
 
 import GraphicsView from "../components/GraphicsView.jsx";
+import HouseholdMap from "../components/HouseholdMap.jsx";
 
 
 export const overviewRoute = createRoute({
     getParentRoute: () => rootRoute,
     path: "/overview",
     component: function Overview() {
+        const [selectedResidents, setSelectedResidents] = useState([]);
+        const [selectedHouseholdId, setSelectedHouseholdId] = useState(null);
+        const [selectedResidentIndex, setSelectedResidentIndex] = useState(null);
+        const [householdWindow, setHouseholdWindow] = useState("");
+        const [residentWindow, setResidentWindow] = useState("");
+
+        const handleHouseholdSelect = (household) => {
+            setSelectedHouseholdId(household.id);
+            setSelectedResidents(household.residents);
+        };
+
+
+
         return (
             <>
                 <Navbar title={"MVP Overview"}></Navbar>
                 <div className="overview-container">
                     <div className="map-container">
-                        <h1> Map</h1>
+                        <HouseholdMap
+                            onSelectResidents={setSelectedResidents}
+                            onSelectHousehold={handleHouseholdSelect}
+                            selectedHouseholdId={selectedHouseholdId}
+                        />
                     </div>
                     <div className="graphics-container">
                         <h1> Graphics</h1>
