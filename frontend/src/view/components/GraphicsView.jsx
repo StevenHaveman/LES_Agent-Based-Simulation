b7a6e5cb1ad8a9d7dfc02733d0abd65d2c96b04e
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2025-06-06 20:57:39 +0200

    version of graphics

--- frontend/src/view/components/GraphicsView.jsx
+++ frontend/src/view/components/GraphicsView.jsx
@@ -1,16 +1,3 @@
-/**
- * Component to display multiple simulation graphs.
- *
- * This component renders a collection of `Graphic` components, each displaying
- * a specific aspect of the simulation data.
- *
- * Props:
- * None
- *
- * Returns:
- * A React component that displays a grid of simulation graphs.
- */
-
 import React from "react";
 import "../styles/GraphicsView.css";
 import Graphic from "./Graphic.jsx";
@@ -19,16 +6,16 @@ const GraphicsView = () => {
     return (
         <div className="graphics-view-container">
             <div className="graphic-wrapper">
-                <Graphic title="Solar Panel Price" yAxisKey="solar_panel_price" />
+                <Graphic title="Totaal Beslissingen Dit Jaar" yAxisKey="decisions_this_year_total" />
             </div>
             <div className="graphic-wrapper">
-                <Graphic title="Decisions For Solar Panels" yAxisKey="decisions_this_year" />
+                <Graphic title="Huishoudens met Pakket (Zonnepaneel)" yAxisKey="solar_panel_households" />
             </div>
             <div className="graphic-wrapper">
-                <Graphic title="Households with Solar Panels" yAxisKey="households_with_panels" />
+                <Graphic title="Prijs per Pakket (Warmtepomp)" yAxisKey="heat_pump_price" />
             </div>
             <div className="graphic-wrapper">
-                <Graphic title="Residents for Solar Panels" yAxisKey="residents_for_panels" />
+                <Graphic title="Positieve Beslissingen (Zonnepaneel)" yAxisKey="solar_panel_positive_decisions" />
             </div>
         </div>
     );
