1f5d3dc81ac6444ac56d8f1018adc9af75ac9aee
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2025-06-24 13:22:00 +0200

    parameters check

--- frontend/src/service/OverviewService.js
+++ frontend/src/service/OverviewService.js
@@ -32,7 +32,7 @@ const API_URL = "http://127.0.0.1:5000";
 class OverviewService {
 
     async fetchHouseholds() {
-        const response = await fetch(`${API_URL}/fetch_households`);
+        const response = await fetch(`${API_URL}/households`);
         if (!response.ok) {
             throw new Error("Fetching households failed");
         }
