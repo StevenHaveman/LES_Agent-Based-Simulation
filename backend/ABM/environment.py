e03aef1496ed02a8238f8a7f13ec967723ad8c00
steven.haveman <steven.haveman@hu.nl>
2025-06-24 12:21:19 +0200

    Added mock-up address

--- backend/ABM/environment.py
+++ backend/ABM/environment.py
@@ -341,6 +341,7 @@ class Environment(Model):
 
             hh_data = {
                 "id": household.unique_id, # Assuming Household has unique_id from Mesa Agent
+                "address": "Dorpsstraat " + household.unique_id,
                 "name": f"Household {household.unique_id}", # Or however you identify them
                 "residents": resident_details
             }
