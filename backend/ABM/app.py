4486d102ce0dc6cefde8a66956beb4265034e3a1
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2025-06-25 13:18:42 +0200

    deploy?

--- backend/ABM/app.py
+++ backend/ABM/app.py
@@ -6,14 +6,16 @@ This application provides endpoints to:
 - Retrieve aggregated data for graphical representation.
 - Fetch detailed data about individual households from the last simulation.
 """
+
+from __future__ import annotations
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from main import run_simulation, graphics_data, households_data
+from .main import run_simulation, graphics_data, households_data
 import utilities
 
 from AgentLLMHandler import AgentLLMHandler
 
-from main import toggle_simulation_pause, is_simulation_paused
+from .main import toggle_simulation_pause, is_simulation_paused
 from shared_state import set_delay, get_delay
 import config
 
