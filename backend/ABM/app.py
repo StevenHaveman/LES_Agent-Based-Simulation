1c448c5ef5d02d7d7aa105bc0ed814551677d0bb
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2025-06-25 14:26:17 +0200

    deploy?

--- backend/ABM/app.py
+++ backend/ABM/app.py
@@ -26,7 +26,7 @@ llm_handler = AgentLLMHandler("llama3.1:8b", chosen_config)
 # Configure CORS to allow connections from the frontend
 CORS(app, resources={r"/*": {"origins": [
     "http://localhost:5173",
-    "https://jouw-frontend-url.netlify.app"
+    "https://inno-2025-groep-625.onrender.com"
 ]}}, supports_credentials=True)
 
 
