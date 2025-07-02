adffa46c9170829c7cb2ef3af41ebe72a68bd6eb
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2025-07-02 18:04:26 +0200

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-    base: "/INNO-2025-Groep-625/",
+    base: "/LES_Agent-Based-Simulation/",
     plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
 });
