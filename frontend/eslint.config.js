15e6dd3d1a1ab74d33ad2a9450b35ee7b37a54f9
Broken-Lad <turan@suslu.eu>
2026-04-14 13:30:24 +0200

    Added: New rules for eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,23 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+			"no-unused-vars": "error",
+			"no-magic-numbers": "error",
+			"eqeqeq": "error",
+			"no-console": "error",
+			"no-duplicate-case": "error",
+			"no-duplicate-imports": "error",
+			"prefer-const": "error",
+			"curly": "error",
+			"no-var": "error",
+
+		},
+  },
   pluginReact.configs.flat.recommended,
 ]);
