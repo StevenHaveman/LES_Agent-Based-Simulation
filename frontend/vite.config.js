bd95a51769c2bff05848f1d76992cdd14cac738b
Broken-Lad <turan@suslu.eu>
2026-04-16 13:31:43 +0200

    Removed unused plugin TanStackRouterVite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
     base: '/INNO/',
-    plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
+    plugins: [react()],
 });
