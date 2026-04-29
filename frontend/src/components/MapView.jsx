e893bb9b4c6c918fc141002645832a488b2edd2f
Broken-Lad <turan@suslu.eu>
2026-04-29 16:17:44 +0200

    Added House icons, and grabs households from service

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -2,7 +2,51 @@ import { useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+const defaultIcon = L.icon({ //TODO MAKE A NEW COMPONENT FOR THIS
+    iconUrl: '/INNO/household.png',
+    iconSize: [16, 16],
+    iconAnchor: [12, 16]
+});
+
+const aLabelIcon = L.icon({
+    iconUrl: '/INNO/household_A_Label.png',
+    iconSize: [16, 16],
+    iconAnchor: [12, 16]
+});
+
+const bLabelIcon = L.icon({
+    iconUrl: '/INNO/household_B_Label.png',
+    iconSize: [16, 16],
+    iconAnchor: [12, 16]
+});
+const cLabelIcon = L.icon({
+    iconUrl: '/INNO/household_C_Label.png',
+    iconSize: [16, 16],
+    iconAnchor: [12, 16]
+});
+const dLabelIcon = L.icon({
+    iconUrl: '/INNO/household_D_Label.png',
+    iconSize: [16, 16],
+    iconAnchor: [12, 16]
+});
+const eLabelIcon = L.icon({
+    iconUrl: '/INNO/household_E_Label.png',
+    iconSize: [16, 16],
+    iconAnchor: [12, 16]
+});
+const fLabelIcon = L.icon({
+    iconUrl: '/INNO/household_F_Label.png',
+    iconSize: [16, 16],
+    iconAnchor: [12, 16]
+});
+const gLabelIcon = L.icon({
+    iconUrl: '/INNO/household_G_Label.png',
+    iconSize: [16, 16],
+    iconAnchor: [12, 16]
+});
+
 const MapView = ({ houses }) => {
+    
     useEffect(() => {
         const map = L.map('map').setView([52.086, 4.399], 14);
 
@@ -11,7 +55,34 @@ const MapView = ({ houses }) => {
         }).addTo(map);
 
         houses.forEach(house => {
-            L.marker([house.lat, house.lng]).addTo(map);
+            let icon;
+            switch (house.energieLabel) {
+                case 'A':
+                    icon = aLabelIcon;
+                    break;
+                case 'B':
+                    icon = bLabelIcon;
+                    break;
+                case 'C':
+                    icon = cLabelIcon;
+                    break;
+                case 'D':
+                    icon = dLabelIcon;
+                    break;
+                case 'E':
+                    icon = eLabelIcon;
+                    break;
+                case 'F':
+                    icon = fLabelIcon;
+                    break;
+                case 'G':
+                    icon = gLabelIcon;
+                    break;
+                default:
+                    icon = defaultIcon;
+            }
+
+            L.marker([house.lat, house.lng], { icon }).addTo(map);
         });
 
         return () => {
--- frontend/src/hooks/useMapData.js
+++ frontend/src/hooks/useMapData.js
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
+import overviewService from "../services/OverviewService";
 
 export const useMapData = () => {
     const [houses, setHouses] = useState([]);
 
     useEffect(() => {
-        fetch("http://127.0.0.1:5000/houses")
-            .then(res => res.json())
-            .then(data => setHouses(data))
-            .catch(err => console.error(err));
+        const fetchData = async () => {
+            try {
+                const data = await overviewService.fetchHouseholds();
+
+                const mapped = data.map(h => ({
+                    lat: h.GIS_attributes?.Latitude,
+                    lng: h.GIS_attributes?.Longitude,
+
+                    address: `${h.GIS_attributes?.WoonplaatsNaam} ${h.GIS_attributes?.OpenbareRuimteNaam} ${h.GIS_attributes?.Huisnummer}`,
+
+                    energieLabel: h.GIS_attributes?.Energielabel,
+                }));
+
+                setHouses(mapped);
+            } catch (err) {
+                console.error("Failed to load houses", err);
+            }
+        };
+
+        fetchData();
     }, []);
 
     return houses;
