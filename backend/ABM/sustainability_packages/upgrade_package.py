7251a5d10799f74f575c1b9305c232a9de3d8d33
Dave Hendriks <114178304+DaveHendriksHU@users.noreply.github.com>
2026-05-06 11:52:52 +0200

    397 bug loading gis data error to much houses (#398)
    * Fixed issue of energy label formatting
    
    * Added regex to remove white spaces

--- backend/ABM/sustainability_packages/upgrade_package.py
+++ backend/ABM/sustainability_packages/upgrade_package.py
@@ -41,7 +41,6 @@ class UpgradePackage:
         """Determines if the upgrade package is feasible for a given household based on affordability and whether it represents a non-regressive step in terms of KPI level."""
 
         affordable = income > self.price
-
         current_rank = household.LEVEL_RANK[household.current_kpi_level]
         target_rank = household.LEVEL_RANK[self.target_level]
 
